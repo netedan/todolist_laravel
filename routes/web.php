022b8b785efa912ac2f952ad99a4466d0e688426
RomanKras <krasro.am@gmail.com>
2024-09-22 12:54:30 +0400

    changed route groups

--- routes/web.php
+++ routes/web.php
@@ -6,35 +6,47 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks_store');
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('task_add');
-Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('task_show');
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('task_edit');
-Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('task_update');
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks_destroy');
+Route::controller(TaskController::class)->prefix('tasks')->group(function () {
+    Route::get('/', 'index')->name('tasks');
+    Route::get('/create', 'create')->name('task_add');
+    Route::post('/', 'store')->name('tasks_store');
+    Route::get('/{task}', 'show')->name('task_show');
+    Route::get('/{task}/edit', 'edit')->name('task_edit');
+    Route::put('/{task}', 'update')->name('task_update');
+    Route::delete('/{task}', 'destroy')->name('tasks_destroy');
+});
 
-Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects_store');
-Route::get('/projects/create', [ProjectController::class, 'create'])->name('project_add');
-Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('project_show');
-Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('project_edit');
-Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('project_update');
-Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects_destroy');
+Route::controller(ProjectController::class)->prefix('projects')->group(function () {
+    Route::get('/', 'index')->name('projects');
+    Route::get('/create', 'create')->name('project_add');
+    Route::post('/', 'store')->name('projects_store');
+    Route::get('/{project}', 'show')->name('project_show');
+    Route::get('/{project}/edit', 'edit')->name('project_edit');
+    Route::put('/{project}', 'update')->name('project_update');
+    Route::delete('/{project}', 'destroy')->name('projects_destroy');
+});
 
-Route::get('/users', [UserController::class, 'index'])->name('users');
-Route::post('/users', [UserController::class, 'store'])->name('users_store');
-Route::get('/users/create', [UserController::class, 'create'])->name('user_add');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('user_show');
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('user_edit');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('user_update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users_destroy');
+Route::controller(UserController::class)->prefix('users')->group(function () {
+    Route::get('/users', 'index')->name('users');
+    Route::get('/users/create', 'create')->name('user_add');
+    Route::post('/users', 'store')->name('user_store');
+    Route::get('/users/{user}', 'show')->name('user_show');
+    Route::get('/users/{user}/edit', 'edit')->name('user_edit');
+    Route::put('/users/{user}', 'update')->name('user_update');
+    Route::delete('/users/{user}', 'destroy')->name('users_destroy');
+});
 
-Route::get('/tags', [TagController::class, 'index'])->name('tags');
-Route::post('/tags', [TagController::class, 'store'])->name('tags_store');
-Route::get('/tags/create', [TagController::class, 'create'])->name('tag_add');
-Route::get('/tags/{tag}', [TagController::class, 'show'])->name('tag_show');
-Route::get('/tags/{tag}/edit', [TagController::class, 'edit'])->name('tag_edit');
-Route::put('/tags/{tag}', [TagController::class, 'update'])->name('tag_update');
-Route::delete('/tags/{tag}', [TagController::class, 'destroy'])->name('tags_destroy');
+Route::controller(TagController::class)->prefix('tags')->group(function () {
+    Route::get('/tags', 'index')->name('tags');
+    Route::get('/tags/create', 'create')->name('tag_add');
+    Route::post('/tags', 'store')->name('tags_store');
+    Route::get('/tags/{tag}', 'show')->name('tag_show');
+    Route::get('/tags/{tag}/edit', 'edit')->name('tag_edit');
+    Route::put('/tags/{tag}', 'update')->name('tag_update');
+    Route::delete('/tags/{tag}', 'destroy')->name('tags_destroy');
+});
+
