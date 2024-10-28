d68f629b05f207cc4542c91549a36292dad8af3d
D. Vorozhtsov <vorozhtsov@internet.ru>
2024-10-28 06:13:57 +0400

    task-28: refactor

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -30,7 +30,7 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
-        $project = Project::create([
+        Project::create([
             'name' => $request->input('project_name'),
             'author_id' => $request->input('author_id'),
         ]);
@@ -42,7 +42,6 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        $tasks = $project->tasks;
         $project->load('author', 'tasks');
         return view('/projects/project', ['project' => $project]);
     }
